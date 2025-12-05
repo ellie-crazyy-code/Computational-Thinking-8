8b4dc95af8909e43fe2dc34c99b91fc0793fb936
ellie <elliezeavy@seattleacademy.org>
2025-12-05 19:26:41 +0000

    print ("what is your name?")

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -27,4 +27,6 @@ print("\n\n")
                                                                                           
                                   
 print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print ("I am a dancer and an equestrian")             
+print ("I have two kittens named Mozzy and Mittens")
+print ("I play the violin")
---
+++ jokes.py
@@ -0,0 +1,4 @@
+print ("why did the skeleton stay home from the dance?")
+input ("")
+print ("it had noBODY to go with!!")
+print ("................................................")
